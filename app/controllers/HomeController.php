dad4d533cd52447060a9aaf6c46fe2b73a9bcc09
shaw65 <shaw65@purdue.edu>
2015-10-17 02:37:08 -0400

    stuff

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
-
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
 class HomeController extends BaseController {
-
+	
 	/*
 	|--------------------------------------------------------------------------
 	| Default Home Controller
@@ -17,7 +18,32 @@ class HomeController extends BaseController {
 
 	public function showWelcome()
 	{
-		return View::make('hello');
-	}
+		$names = array();
+		$prices = array();
+		$client = new Client();
+		$crawler = $client->request('GET', 'http://www.walmart.com/search/?query=milk&grid=true&');
+		$results = $crawler->filter('.tile-grid-unit-wrapper')->each(function (Crawler $node, $i) use (&$prices, &$names){
+			$p = $node->filter('.tile-price');
+			$n = $node->filter('.tile-heading');
+			$price = explode(" ",$p->text());
+			$prices[] = $price[3];
+			$names[] = $n->text();
+   			//echo ($n->text());
+   			//echo '</br>';
 
+		});
+		//var_dump($prices);
+		//var_dump($names);
+		for($i = 0;$i<count($prices);$i++){
+			if(strpos($prices[$i], '$') !== FALSE){
+				echo $names[$i] . "  <b>" . $prices[$i] . "</b></br>";
+			}else{
+
+				unset($prices[$i]);
+			}
+			
+		}
+		//var_dump($test->html());
+		//return View::make('hello', array('lol' => 'hey'));
+	}
 }
