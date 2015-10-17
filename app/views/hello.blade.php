95c098c9c35cf38144a6120668c26e3d0a051f06
Joey Savastano <jsavasta@purdue.edu>
2015-10-17 13:12:41 -0400

    I might've written an infinite loop

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -20,8 +20,8 @@ h1 {
     <h1><u>Groceries</u></h1>      
   </div>    
 </div>
-<table id="productTable" class="table table-hover"> 
 	<div class="welcome">
+<table id="productTable" class="table table-hover"> 
 	<script>
 		var table = document.getElementById("productTable");
 		var row = table.insertRow(0);
@@ -29,10 +29,12 @@ h1 {
 		row.insertCell(1).innerHTML = "Price";
 		row.insertCell(2).innerHTML = "Store";
     	var items = <?php echo json_encode($items); ?>;
+ 		var j = 0;
     	for(var i = 0; i < items.length; i++) {
-		    var row = table.insertRow(1+i);
+    	    var row = table.insertRow(j+1);
 		    var cell1 = row.insertCell(0);
 		    var cell2 = row.insertCell(1);
+		    j = 0;
 		    cell1.innerHTML = items[i].name;
 		    cell2.innerHTML = items[i].price;
 		}
@@ -46,10 +48,6 @@ h1 {
 	{{ HTML::style('css/style.css'); }}
 </head>
 <body>
-		<?php
-			for($i = 0; $i< 12; $i++){
-			}
-		?>
 	</div>
 </body>
 </html>
