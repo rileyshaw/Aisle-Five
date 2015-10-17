4f44757a21b09b384eca99b9d9b58d0d56bf14c9
Joseph S Savastano <joseph@pal-nat186-108-60.itap.purdue.edu>
2015-10-17 18:18:05 -0400

    I HATE THIS

--- app/views/scripts.js
+++ app/views/scripts.js
@@ -42,36 +42,3 @@ function sort(how) {
     	for(var i = 0; i < items.length; i++) 
 		    $('table tr:last').after("<tr><td class='deleterow'><div class='glyphicon glyphicon-remove'></div></td>"+"<td>"+items[i].name +"</td><td>"+"$"+items[i].price +"</td><td>"+items[i].storeName+"</tr>");
 	}
-<<<<<<< HEAD
-
-=======
-function makeTable() {
-	var table = document.getElementById("productTable");
-    	var items = "";
- 		var j = 0;
- 		for(var i = 0; i < items.length; i++)
- 			items[i].price = parseFloat(items[i].price + "<br>");
- 		for(var i = 0; i < items.length; i++) {
- 			for(var j = i; j < items.length; j++) {
- 				if(items[i].price > items[j].price) {
- 					temp = items[i];
- 					items[i] = items[j];
- 					items[j] = temp;
- 				}
- 			}
- 		}
-    	for(var i = 0; i < items.length; i++) 
-		    $('table tr:last').after("<tr><td class='deleterow'><div class='glyphicon glyphicon-remove'></div></td>"+"<td>"+items[i].name +"</td><td>"+"$"+items[i].price +"</td><td>"+items[i].storeName+"</tr>");
-		$(".deleterow").on("click", function(){
-		var $killrow = $(this).parent('tr');
-    	$killrow.addClass("danger");
-			$killrow.fadeOut(2000, function(){
-    			$(this).remove();
-			});
-	});
-}
-function addToTable() {
-		var table = document.getElementById("productTable");
-			table.deleteRow(0);			
-		}
->>>>>>> origin/master
